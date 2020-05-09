549948887310fcfbc5938e74cd94080c8574f344
shah@idk.uno <5903821+smashah@users.noreply.github.com>
2020-05-09 08:44:26 +0100

    updated test data

--- test/index.ts
+++ test/index.ts
@@ -3,21 +3,14 @@ const mime = require('mime-types');
 const fs = require('fs');
 
 async function test() {
+  //The absolute minimum data required to decrypt a file
     const message = {
         "type": "image",
-        "t": 1587248609, 
-        "clientUrl": "https://mmg-fna.whatsapp.net/d/f/Atdfg_vtnm1GexGZY4Qllcsnvk9bQQs9Wihm0GCa8Ly9.enc",
-        "directPath": "/v/t62.7118-24/31297007_2889015684498573_8260880620120850949_n.enc?oh=60fdee4af4bce909ff19a8da7251c3ea&oe=C4BA3D89",
+        "clientUrl": "https://mmg-fna.whatsapp.net/d/f/ArWPNrfRA-dsdarFjaUKLmdH3dsbqlAa46uu_185IrdX.enc",
         "mimetype": "image/jpeg",
-        "filehash": "omlsH6BMAjnADg0gO5kSPj1vxBQ8wkSkS9CdWxTTbj0=",
-        "uploadhash": "6sDgacjBXn0CMJmk4Ywu2d5Srzff9o5/tdLZYDuTIpI=",
-        "size": 104781,
-        "mediaKey": "hd69/kqRLig4EZrePm7sURqUAtC1mDfiftiUbl8rQCw=",
-        "mediaKeyTimestamp": 1587248601,
-        "width": 800,
-        "height": 600
+        "mediaKey":  "qHQbszo9ErZQDo396CepreN9UAhCU6kz2l2zu8vjsYU=",
       }
-    const filename = `${message.t}.${mime.extension(message.mimetype)}`;
+    const filename = `${Date.now()}.${mime.extension(message.mimetype)}`;
     const mediaData = await decryptMedia(message,"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.92 Safari/537.36");
     fs.writeFile(filename, mediaData, function(err) {
       if (err) {
